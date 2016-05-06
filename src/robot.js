f6dbce9380532a5ebaec41d00da3caad1cffe23e
Pete Redmond <redmondp@gmail.com>
2016-05-06 10:08:36 +0100

    robot setOrientation and move methods

--- src/robot.js
+++ src/robot.js
@@ -7,11 +7,73 @@ class Robot {
   * @param  {String} orientation : starting orientation of Robot
   * @param  {Grid Object} grid
   */
-  constructor(x, y, orientation) {
+  constructor(x, y, orientation, grid) {
     this.x = x;
     this.y = y;
     this.orientation = orientation;
     this.lost = false;
+    this.grid = grid;
+  }
+
+  /**
+   * setOrientation of robot, this can either be +90 or
+   * -90 degrees, the clockwise parameter specifies
+   * which direction to turn
+   * @param {Boolean} clockwise
+   */
+  setOrientation(clockwise) {
+
+    let  newOrientation;
+
+    switch (this.orientation) {
+      case 'E':
+        newOrientation = (clockwise) ? 'S' : 'N';
+        break;
+      case 'W':
+        newOrientation = (clockwise) ? 'N' : 'S';
+        break;
+      case 'S':
+        newOrientation = (clockwise) ? 'W' : 'E';
+        break;
+      case 'N':
+        newOrientation = (clockwise) ? 'E' : 'W';
+        break;
+    }
+
+    this.orientation  = newOrientation;
+  }
+
+  move(direction = this.orientation) {
+    let currentX = this.x;
+    let currentY = this.y;
+
+    // if robot is lost do nothing
+    if (this.lost) {
+      return false;
+    }
+
+    switch (direction) {
+      case 'E':
+        this.x++;
+        break;
+      case 'W':
+        this.x--;
+        break;
+      case 'N':
+        this.y++;
+        break;
+      case 'S':
+        this.y--;
+        break;
+    }
+
+    // robot is off the grid
+    if (this.grid.isOutOfBounds(this.x, this.y)) {
+      this.x = currentX;
+      this.y = currentY;
+      this.lost = true;
+    }
+
   }
 
 };
--- test/robot.spec.js
+++ test/robot.spec.js
@@ -1,18 +1,44 @@
 const expect = require('chai').expect;
 
 const Robot = require('./../src/robot');
+const Grid = require('./../src/grid');
 
 describe('Robot class', function() {
 
   let robot;
+  let grid;
 
   it('instance of Robot class', () => {
-    robot = new Robot(0, 3, 'S');
+    grid = new Grid(5, 3);
+    robot = new Robot(0, 3, 'S', grid);
 
     expect(robot).to.be.instanceOf(Robot);
     expect(robot.x).to.equal(0);
     expect(robot.y).to.equal(3);
     expect(robot.orientation).to.equal('S');
+  });
+
+  it('should change orientation', () => {
+    grid = new Grid(5, 3);
+    robot = new Robot(3, 4, 'E', grid);
+    robot.setOrientation(true);
+    expect(robot.orientation).to.equal('S');
+    robot.setOrientation(false);
+    expect(robot.orientation).to.equal('E');
+  });
+
+  it('should move one cell in the direction you are facing', () => {
+    grid = new Grid(5, 3);
+    robot = new Robot(1, 1, 'E', grid);
+    robot.move();
+    expect(robot.x).to.equal(2);
+  });
+
+  it('a robot should be set to lost if it goes out of the grid', () => {
+    robot = new Robot(0, 0, 'S', grid);
+    robot.move()
+    expect(robot.x).to.equal(0);
+    expect(robot.lost).to.be.true;
   });
 
 });
